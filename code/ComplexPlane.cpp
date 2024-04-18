42dc51d63df8f752b9b4cdc04b45bdb8b77facfc
Your Name <you@example.com>
2024-04-17 18:21:18 -0700

    music working

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -49,7 +49,9 @@ void ComplexPlane::run() {
     }
     Sound sound;
     sound.setBuffer(buffer);
+    sound.setLoop(true);
     sound.play();
+    
 
     Text text("", font, DEFAULT_CHARACTER_SIZE);
     text.setFillColor(DEFAULT_TEXT_COLOR);
@@ -57,6 +59,8 @@ void ComplexPlane::run() {
     
 
     while (window.isOpen()) {
+        
+        
         
         handleEvent(window, update);
 
